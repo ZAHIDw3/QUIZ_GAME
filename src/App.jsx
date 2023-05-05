330c771afe1acf467ab3107c926edf8c64b4243b
Zahid de Jesus Avalos Hernandez <l19350283@tuxtepec.tecnm.mx>
2023-05-04 22:06:36 -0600

    Pagina de Home lista y creacion de un useState para poder moverse entre Home, Capture y Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,57 @@
-
+import { useState } from "react";
+import HomePagina from "./components/HomePagina";
 function App() {
-
+  const [pagina, setPagina] = useState('Home')
+  console.log(pagina)
   return (
-    <div>
-      <h3>Quiz Game</h3>
+    <div className="container mt-2">
+
+      {
+        pagina === 'Home' && (
+          <div>
+            <HomePagina
+              setPagina={setPagina} />
+          </div>
+        )
+      }
+      {
+        pagina === 'Capture' && (
+          <div>
+            <h1>Capture</h1>
+            <button
+              type="button"
+              class="btn btn-warning"
+              onClick={()=>setPagina('Home')}
+              style={{
+                padding: "14px 20px",
+                border: "none",
+                width: "100%",
+              }}
+            >Home</button>
+          </div>
+        )
+      }
+      {
+
+        pagina === 'Game' && (
+          <div>
+            <h1>Game</h1>
+            <button
+              type="button"
+              class="btn btn-success"
+              onClick={()=>setPagina('Home')}
+              style={{
+                padding: "14px 20px",
+                border: "none",
+                width: "100%",
+              }}
+            >Home</button>
+          </div>
+        )
+      }
+
+
+
     </div>
   )
 }
---
+++ src/components/HomePagina.jsx
@@ -0,0 +1,37 @@
+
+const HomePagina =({setPagina}) => {
+
+    return (
+        <div>
+            <h1
+                style={{
+                    textAlign: "center"
+                }}>Quiz Game</h1>
+
+            <button
+                type="button"
+                className="btn btn-primary"
+                onClick={()=>setPagina('Capture')}
+                style={{
+                    padding: "14px 20px",
+                    border: "none",
+                    width: "100%",
+                    margin: "50px 0px",
+                }}
+            >Capture</button>
+
+            <button
+                type="button"
+                className="btn btn-info"
+                onClick={()=>setPagina('Game')}
+                style={{
+                    padding: "14px 20px",
+                    border: "none",
+                    width: "100%",
+                }}
+            >Game</button>
+        </div>
+    )
+}
+
+export default HomePagina
